e5dc4527b4b045037d5f1ab897d7ddaff148b0f4
Naomi Mardon <naomimardon@Naomis-MBP.broadband>
2023-03-18 17:21:19 +0000

    added contact info section on contact page

--- src/components/Info/info.js
+++ src/components/Info/info.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
@@ -25,7 +25,7 @@ const Info = () => {
               <p>I am currently enrolled in a Front-End Web Development Bootcamp, due to finish at the end of March 2023. During this rigorous and fast-paced course, I have learnt the practical and technical skills needed to build robust front-end web applications.</p>
               <p>I have gained proficiency in numerous technologies, including HTML, CSS, JavaScript, Node.js and React, and plan to build on these skills in the coming months.</p>
               <p>Please lake a look at the
-                <span><NavLink to="/projects" end> Projects page</NavLink></span> to view a selection of my work.
+                <span><Link to="/projects" end> Projects page</Link></span> to view a selection of my work.
               </p>
               <p>Having worked in fundraising for the past twelve years, leading teams to achieve excellence and consistently deliver growth, I believe that my skills and personality are well suited to a career in web development. I am highly organised with good problem-solving skills and the ability to manage a wide variety of projects at one time. I look forward to putting these skills to use as I continue on this journey!</p>
             </Typography>
